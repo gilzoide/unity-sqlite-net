59ada2fcdcf86810b5a02dfa214992c1e70a7c28
gilzoide <gilzoide@gmail.com>
2025-01-12 09:34:30 -0300

    Add TestSqliteAsync script, the async version of TestSqlite

--- Samples~/Readme/TestSqliteAsync.cs
+++ Samples~/Readme/TestSqliteAsync.cs
@@ -3,59 +3,50 @@ using UnityEngine;
 
 namespace Gilzoide.SqliteNet.Samples.Readme
 {
-    // The library contains simple attributes that you can use
-    // to control the construction of tables, ORM style
-    public class Player
+    public class TestSqliteAsync : MonoBehaviour
     {
-        [PrimaryKey, AutoIncrement]
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-
-    public class TestSqlite : MonoBehaviour
-    {
-        void Start()
+        async void Start()
         {
             // 1. Create a connection to the database.
             // The special ":memory:" in-memory database and
             // URIs like "file:///somefile" are also supported
             var path = $"{Application.persistentDataPath}/MyDb.db";
             Debug.Log($"Opening database '{path}'");
-            var db = new SQLiteConnection(path);
+            var db = new SQLiteAsyncConnection(path);
 
             // 2. Once you have defined your entity, you can automatically
-            // generate tables in your database by calling CreateTable
-            db.CreateTable<Player>();
+            // generate tables in your database by calling CreateTableAsync
+            await db.CreateTableAsync<Player>();
 
-            // 3. You can insert rows in the database using Insert
+            // 3. You can insert rows in the database using InsertAsync
             // The Insert call fills Id, which is marked with [AutoIncremented]
             var newPlayer = new Player
             {
                 Name = "gilzoide",
             };
-            db.Insert(newPlayer);
+            await db.InsertAsync(newPlayer);
             Debug.Log($"Player new ID: {newPlayer.Id}");
             // Similar methods exist for Update and Delete.
 
             // 4.a The most straightforward way to query for data
             // is using the Table method. This can take predicates
             // for constraining via WHERE clauses and/or adding ORDER BY clauses
-            var query = db.Table<Player>().Where(p => p.Name.StartsWith("g"));
+            var query = await db.Table<Player>().Where(p => p.Name.StartsWith("g")).ToListAsync();
             foreach (Player player in query)
             {
                 Debug.Log($"Found player named {player.Name} with ID {player.Id}");
             }
 
-            // 4.b You can also make queries at a low-level using the Query method
-            var players = db.Query<Player>("SELECT * FROM Player WHERE Id = ?", 1);
+            // 4.b You can also make queries at a low-level using the QueryAsync method
+            var players = await db.QueryAsync<Player>("SELECT * FROM Player WHERE Id = ?", 1);
             foreach (Player player in players)
             {
                 Debug.Log($"Player with ID 1 is called {player.Name}");
             }
 
-            // 5. You can perform low-level updates to the database using the Execute
-            // method, for example for running PRAGMAs or VACUUM
-            db.Execute("VACUUM");
+            // 5. You can perform low-level updates to the database using the
+            // ExecuteAsync method, for example for running PRAGMAs or VACUUM
+            await db.ExecuteAsync("VACUUM");
         }
     }
 }
