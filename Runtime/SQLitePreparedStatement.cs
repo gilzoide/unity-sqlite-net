44049d377dde51a28f8bb4aedc9b322e85c5166d
gilzoide <gilzoide@gmail.com>
2025-06-28 13:16:35 -0300

    Update copyright notice year

--- Runtime/SQLitePreparedStatement.cs
+++ Runtime/SQLitePreparedStatement.cs
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Gil Barbosa Reis
+ * Copyright (c) 2025 Gil Barbosa Reis
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
